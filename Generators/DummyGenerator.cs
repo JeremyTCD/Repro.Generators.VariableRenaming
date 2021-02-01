d413e7da85a6ac2bb25a902988c0866d2e2678ef
JeremyTCD <jeremytanchongdao@gmail.com>
2021-02-01 14:04:21 +0800

    Initial

---
+++ Generators/DummyGenerator.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Generators
+{
+    [Generator]
+    public class DummyGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            string dummySource = $@"using System;
+
+namespace Repro.Generators.VariableRenaming
+{{ 
+    public static class DummyMethods
+    {{
+        public static void {((SyntaxReceiver)context.SyntaxReceiver).MethodName}(string message){{
+            Console.Write(message);
+        }}
+    }}
+}}
+";
+
+            context.AddSource("DummyMethods.Generated.cs", dummySource);
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new SyntaxReceiver());
+        }
+
+        private class SyntaxReceiver : ISyntaxReceiver
+        {
+            public string MethodName;
+
+            public void OnVisitSyntaxNode(SyntaxNode syntaxNode)
+            {
+                if (syntaxNode is MemberAccessExpressionSyntax memberAccessExpressionSyntax &&
+                    memberAccessExpressionSyntax.Expression is IdentifierNameSyntax identifierNameSyntax &&
+                    identifierNameSyntax.Identifier.ValueText == "DummyMethods")
+                {
+                    MethodName = memberAccessExpressionSyntax.Name.Identifier.ValueText;
+                }
+            }
+        }
+    }
+}
---
+++ Repro.Generators.VariableRenaming/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Repro.Generators.VariableRenaming
+{
+    class Program
+    {
+        static void Main()
+        {
+            string message = "Hello";
+
+            DummyMethods.Test(message);
+        }
+    }
+}
